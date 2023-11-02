5c5036a9435102c3a45571252abc203faedbf92c
Dominik.Kotecki <dominikus1910@gmail.com>
2023-11-02 18:56:38 +0100

    add countter

--- src/DevNewsBot.App/Pages/Index.cshtml.cs
+++ src/DevNewsBot.App/Pages/Index.cshtml.cs
@@ -7,7 +7,7 @@ namespace DevNewsBot.App.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
+    private static int _count = 0;
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
@@ -15,8 +15,10 @@ public class IndexModel : PageModel
 
     public async Task<IActionResult> OnGetAsync()
     {
-        return Request.IsHtmx()
-            ? Content("<span>Hello, World!</span>", "text/html")
+        var isHtmx = Request.IsHtmx();
+        Interlocked.Increment(ref _count);
+        return isHtmx
+            ? Content($"<span>Hello, World! {_count}</span>", "text/html")
             : Page();
     }
     
